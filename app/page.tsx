e4b42b1a29ae52f287bd34dc8eaae2a46878be12
rahmat noori <rahmatnoori48@gmail.com>
2024-12-28 12:50:32 +0400

    old code replaced with new ones

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,37 @@
-import Grid from "@/components/Grid";
-import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
+"use client";
 
-export default function Home() {
+import { navItems } from "@/data";
+
+import Hero from "@/components/Hero";
+// import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Clients from "@/components/Clients";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
+import dynamic from "next/dynamic";
+
+const BrowserComponent = dynamic(() => import("@/components/Grid"), {
+  ssr: false,
+});
+
+const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center overflow-hidden flex-col mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={[
-            { name: "Home", link: "/", icon: <FaHome /> },
-            { name: "About", link: "/about" },
-            { name: "Contact", link: "/contact" },
-          ]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid />
+        <BrowserComponent />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
-}
+};
+
+export default Home;
